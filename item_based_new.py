0e74bd729b1e218220e856b501cdb18312f0f284
rongrong.li <basement_girl@163.com>
2017-05-19 16:54:25 +0800

    继续优化

--- item_based_new.py
+++ item_based_new.py
@@ -22,6 +22,14 @@ def topKMatches(trainSet, presentUserid, presentItemid, sim,k=30):
 #neighborSimAndItems此时是以元组为元素的列表。每个元组包括包括邻居项目与当前项目的相似度以及邻居项目id。
 
 
+def getUserAverage(trainSet, userid):
+    count = 0
+    sum = 0
+    for item in trainSet[userid]:
+        sum = sum + trainSet[userid][item]
+        count = count+1
+    return sum/count
+
 
 def getItemAverage(trainSet, itemid):
     all_scores_for_item=[] #存放所有用户对这个项目的评分。
@@ -47,17 +55,21 @@ def getRating(trainSet, presentUserid, presentItemid,sim):
             return sum(all_scores_for_item)/len(all_scores_for_item)
         #如果当前用户是新用户，但当前商品不是新项目，则返回当前商品的平均评分。
     else:
-        #如果当前用户不是新用户。则其一定有评价过的其他项目。这就有了当前物品的备选邻居。
-        neighborSimAndItems = topKMatches(trainSet, presentUserid, presentItemid, sim)
-        s=0  #代表分子。即当前用户对每个近邻物品的评分乘上该近邻物品与当前物品的相似度。然后再求和的结果。
-        simSum = 0
-        avgOfPresentItem=sum(all_scores_for_item)/len(all_scores_for_item)
+        if len(all_scores_for_item)==0:
+            return getUserAverage(trainSet, presentUserid)
+        #如果当前用户不是新用户，但当前商品是新项目。则返回当前用户的平均评分
+        else:
+        # 如果当前用户不是新用户。当前商品又不是新商品。则按正常流程来。
+            neighborSimAndItems = topKMatches(trainSet, presentUserid, presentItemid, sim)
+            s=0  #代表分子。即当前用户对每个近邻物品的评分乘上该近邻物品与当前物品的相似度。然后再求和的结果。
+            simSum = 0
+            avgOfPresentItem=sum(all_scores_for_item)/len(all_scores_for_item)
 
-        for i in neighborSimAndItems:
-            avgOfNeighborItem = getItemAverage(trainSet, i[1])
-            s = s + abs(i[0]) * (trainSet[presentUserid][i[1]]-avgOfNeighborItem)
-            simSum+=abs(i[0])
-        return avgOfPresentItem+s/simSum
+            for i in neighborSimAndItems:
+                avgOfNeighborItem = getItemAverage(trainSet, i[1])
+                s = s + abs(i[0]) * (trainSet[presentUserid][i[1]]-avgOfNeighborItem)
+                simSum+=abs(i[0])
+            return avgOfPresentItem+s/simSum
 
 
 def getAllUserRating(fileTrain, fileTest, fileResult,sim):
